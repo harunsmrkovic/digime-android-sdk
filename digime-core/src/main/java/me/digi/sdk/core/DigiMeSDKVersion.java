f43e7b28c000764706647ca4ae667cd4d52edb2d
Krzysztof Skrzynecki <krzysztof@digi.me>
2017-12-06 09:22:47 +0000

    Increment SDK version

--- digime-core/src/main/java/me/digi/sdk/core/DigiMeSDKVersion.java
+++ digime-core/src/main/java/me/digi/sdk/core/DigiMeSDKVersion.java
@@ -5,5 +5,5 @@
 package me.digi.sdk.core;
 
 public final class DigiMeSDKVersion {
-    public static final String VERSION = "1.1.3-SNAPSHOT";
+    public static final String VERSION = "1.1.4-SNAPSHOT";
 }
