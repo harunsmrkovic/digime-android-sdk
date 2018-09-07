6b33dda04c96540047be050f744b18339caad75d
Hamish Maclean <hamish@digi.me>
2018-09-07 15:48:47 +0100

    [DIGI-11459] postbox demo app: show postbox id on success

--- examples/postbox/src/main/java/me/digi/examples/postbox/MainActivity.java
+++ examples/postbox/src/main/java/me/digi/examples/postbox/MainActivity.java
@@ -4,19 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
-import me.digi.examples.postbox.ActivityUtils;
-import me.digi.examples.postbox.R;
+import me.digi.sdk.core.CreatePostboxSession;
 import me.digi.sdk.core.DigiMeClient;
 import me.digi.sdk.core.SDKCallback;
 import me.digi.sdk.core.SDKException;
 import me.digi.sdk.core.SDKResponse;
-import me.digi.sdk.core.session.CASession;
+import me.digi.sdk.core.session.SessionResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private View progressBar;
     private View dimScreen;
+    private TextView postboxId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +26,13 @@ public class MainActivity extends AppCompatActivity {
 
         dimScreen = findViewById(R.id.dim_screen);
         progressBar = findViewById(R.id.progress_bar);
+        postboxId = findViewById(R.id.postbox_id);
 
         View createPostboxButton = findViewById(R.id.create_postbox_button);
         createPostboxButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showProgressBar();
+                //showProgressBar();
                 createPostbox();
             }
         });
@@ -43,17 +45,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createPostbox() {
-        DigiMeClient.getInstance().createPostbox(this, new SDKCallback<CASession>() {
+        DigiMeClient.getInstance().createPostbox(this, new SDKCallback<SessionResult>() {
             @Override
-            public void succeeded(SDKResponse<CASession> result) {
-                System.out.println("hmac create postbox success");
-                hideProgressBar();
+            public void succeeded(SDKResponse<SessionResult> result) {
+                if (result.body instanceof CreatePostboxSession) {
+                    postboxId.setText(((CreatePostboxSession)(result.body)).postboxId);
+                }
+                //hideProgressBar();
             }
 
             @Override
             public void failed(SDKException exception) {
-                System.out.println("hmac create postbox failure");
-                hideProgressBar();
+                System.out.println("create postbox failure: "+exception.toString());
+                //hideProgressBar();
             }
         });
     }
